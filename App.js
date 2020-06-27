debbbe116ff09db65220f57d4e8e288d28c2b24f
LucasOlivel <olivel890@gmail.com>
2020-06-26 21:15:03 -0300

    'Final'

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import PrevisaoItem from './componentes/PrevisaoItem';
 
 export default function App() {
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt&units=metric&q=";
+  const apiKey = "b54e699bd9eb3326fd49815e887fd16e";
+
+  const obterPrevisoes = () => {
+    setPrevisoes([]);
+    const target = endPoint + cidade + '&appid=' + apiKey;
+    fetch(target)
+      .then((dados => dados.json()))
+      .then(dados => setPrevisoes(dados["list"]));
+  }
+
+  const[cidade, setCidade] = useState('');
+  const[previsoes, setPrevisoes] = useState([]);
+  const capturarCidade = (cidade) => {
+    setCidade(cidade)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput
+          style = {styles.nomeCidade}
+          placeholder="Digite o nome de uma cidade"
+          onChangeText={capturarCidade}
+        />
+        <Button 
+          title="OK"
+          onPress={obterPrevisoes}
+        />
+      </View>
+      <FlatList 
+        data={previsoes}
+        renderItem={
+          previsao => <PrevisaoItem previsao={previsao.item}></PrevisaoItem>
+        }
+      />
     </View>
   );
 }
@@ -16,6 +50,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40
+  },
+  entrada: {
+    flexDirection: 'column'
+  },
+  nomeCidade: {
+    padding: 10,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    marginBottom: 4,
+    fontSize: 18
   },
 });
